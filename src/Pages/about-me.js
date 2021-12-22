7ec7931841b0421091ba535604f26083c2503ab1
David Silveira <daviewave@gmail.com>
2021-12-22 07:14:42 -0500

    added very basic frame for what the about me page will look like

---
+++ src/Pages/about-me.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div className="page-container">
+      <h2 className="page-heading">ABOUT ME</h2>
+      <div className="page-content d-flex flex-row">
+        <p className="p">some text here</p>
+      </div>
+    </div>
+  );
+}
