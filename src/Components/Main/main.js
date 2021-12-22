a2dd30502576955b4bf0b28b0f775bedffdb0691
David Silveira <daviewave@gmail.com>
2021-12-22 06:40:07 -0500

    added code for the 'main' section of the components folder

---
+++ src/Components/Main/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="main-container">
+      <h1 className="name">David Silveira</h1>
+      <h2 className="certification">Full Stack Web Developer</h2>
+    </div>
+  );
+}
