5932c824b55ac2b07ca751f34af6fbf8f8605567
David Silveira <daviewave@gmail.com>
2021-12-22 07:46:20 -0500

    added very basic frame for what the home page will look like

---
+++ src/Pages/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="page-container">
+      <h2 className="page-heading">David Silveira's Portfolio</h2>
+      <div className="page-content">
+        <p className="p">some text here</p>
+      </div>
+    </div>
+  );
+}
